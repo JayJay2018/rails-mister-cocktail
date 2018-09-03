ca006e5bdbbb7e675dd94b80b066afcfdc847bba
Jascha Rafael Franke <hola@11once.de>
2018-09-03 14:14:28 +0200

    Add logic in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,16 @@
 require 'open-uri'
 require 'json'
 
+puts "Dropping database!"
+Ingredient.destroy_all
+puts "Done."
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 file = open(url).read
 ingredients = JSON.parse(file)
 
+puts "Creating ingredients!"
+
 ingredients["drinks"].each do |ingredient|
   Ingredient.create(name: ingredient["strIngredient1"])
 end
+puts "Finished! #{Ingredient.all.count} Ingredients have been created."
