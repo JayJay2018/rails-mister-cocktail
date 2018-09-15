93e503883199427240e9dcaafb8662ee98c71a17
Jascha Rafael Franke <hola@11once.de>
2018-09-15 19:05:11 +0200

    Fix policy for doses

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,24 +1,41 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [ :create ]
+  before_action :set_dose, only: [ :destroy ]
+
 
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
+    # @dose.cocktail.user = current_user
     @dose.cocktail = @cocktail
+    authorize @dose
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render 'cocktails/show'
+      redirect_to cocktail_path(@cocktail), notice: "Creating failed. Please try again."
     end
   end
 
   def destroy
-    @dose = Dose.find(params[:id])
-    @cocktail = @dose.cocktail
-    @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    @dose.cocktail = @cocktail
+    authorize @dose
+     if @dose.destroy
+      redirect_to cocktails_path, notice: "You succesfully deleted #{@dose.ingredient.name}."
+    else
+      redirect_to cocktail_path(@cocktail), notice: "Something went wrong, please try again."
+    end
   end
 
   private
+
+  def set_cocktail
+    @cocktail = policy_scope(Cocktail).find(params[:cocktail_id])
+    authorize @cocktail
+  end
+
+  def set_dose
+    @dose = policy_scope(Dose).find(params[:id])
+    authorize @dose
+  end
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
